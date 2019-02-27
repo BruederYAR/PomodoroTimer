a53a3c87a89c9639b1a14a7d626cf3f529102259
Brueder <brueder111@gmail.com>
2019-02-28 02:42:04 +0400

    first commit

--- PomodoroTimer/Form1.Designer.cs
+++ PomodoroTimer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PomodoroTimer
 {
-    partial class Form1
+    partial class Pomodoro
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,12 +29,82 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pomodoro));
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.startButt = new System.Windows.Forms.Button();
+            this.stopButt = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timerLabel.Location = new System.Drawing.Point(66, 30);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(57, 63);
+            this.timerLabel.TabIndex = 0;
+            this.timerLabel.Text = "0";
+            // 
+            // startButt
+            // 
+            this.startButt.BackColor = System.Drawing.Color.Red;
+            this.startButt.Location = new System.Drawing.Point(12, 118);
+            this.startButt.Name = "startButt";
+            this.startButt.Size = new System.Drawing.Size(76, 40);
+            this.startButt.TabIndex = 1;
+            this.startButt.Text = "START";
+            this.startButt.UseVisualStyleBackColor = false;
+            this.startButt.Click += new System.EventHandler(this.startButt_Click);
+            // 
+            // stopButt
+            // 
+            this.stopButt.BackColor = System.Drawing.Color.Red;
+            this.stopButt.Location = new System.Drawing.Point(100, 118);
+            this.stopButt.Name = "stopButt";
+            this.stopButt.Size = new System.Drawing.Size(76, 40);
+            this.stopButt.TabIndex = 2;
+            this.stopButt.Text = "STOP";
+            this.stopButt.UseVisualStyleBackColor = false;
+            this.stopButt.Click += new System.EventHandler(this.stopButt_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Pomodoro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(188, 174);
+            this.Controls.Add(this.stopButt);
+            this.Controls.Add(this.startButt);
+            this.Controls.Add(this.timerLabel);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Pomodoro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Pomodoro";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button startButt;
+        private System.Windows.Forms.Button stopButt;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PomodoroTimer/Form1.cs
+++ PomodoroTimer/Form1.cs
@@ -7,14 +7,130 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Media;
 
 namespace PomodoroTimer
 {
-    public partial class Form1 : Form
+    public partial class Pomodoro : Form
     {
-        public Form1()
+        public Pomodoro()
         {
             InitializeComponent();
+        }
+
+        SoundPlayer simple = new SoundPlayer(@"C:\Windows\media\tada.wav"); //Звук окончания
+        Color WorkColor = Color.Red; //Цвет работы
+        Color RestColor = Color.Lime; //Цвет отдыха
+
+        public int TimeSec =  0; //Время в секундах
+        public int TimeMin = 0; //Время в минутах
+        public int TimeJobLimit = 25; //Лимит времени работы (минуты)
+        public int TimeRestLimit = 5; //Лимит времени работы (минуты)
+
+        public bool Pause = false; //Флаг паузы
+        public bool Rest = false; //Флаг отдыха
+
+        private void timer1_Tick(object sender, EventArgs e) 
+        {
+            TimeOver(); //Проаерять не закончилось ли время
+            Print(timerLabel, ref TimeSec, ref TimeMin); // Вывести время в форму
+            TimeSec++; //Прибавить тик
+        }
+
+        void Print(Label timer, ref int TimeSec, ref int TimeMin) //Метод вывода времени на форму
+        {
+            const int Min = 60; //Значение минуты
+            if (TimeMin == 0) //Если не прошло минуты
+            {
+                if (TimeSec > 9) //Если не двухзначное число
+                    timer.Left = 47; //Перемещяем
+                else
+                    timer.Left = 66;
+
+                timer.Text = TimeSec.ToString(); //Выводим
+                if (TimeSec == Min) //Проверка на минуту
+                {
+                    TimeMin++;
+                    TimeSec = 0;
+                }
+            }
+            else 
+            {
+                timer.Left = 40;
+                timer.Text = $"{TimeMin}:{TimeSec}"; //Выводим вместе с минутами
+                if (TimeSec == 60) //Проверка на минуту
+                {
+                    TimeMin++;
+                    TimeSec = 0;
+                }
+            }
+        }
+        void Stop() //Сброс всех значений 
+        {
+            timer1.Enabled = false;
+            startButt.Text = "START";
+            Pause = false;
+            TimeSec = 0;
+            TimeMin = 0;
+            Print(timerLabel, ref TimeSec, ref TimeMin);
+        }
+        void TimeOver() //Проверка на окончание времени
+        {
+            if(TimeMin == TimeJobLimit && !Rest) //Если время работы окончено и мы не отдыхаем
+            {
+                Stop(); //Останавливаем
+                simple.Play(); //Проигрываем звук
+                stopButt.Text = "SKIP";  //Меняем имя кнопки
+                MessageBox.Show("Time is over, take a rest", "Time is over", MessageBoxButtons.OK, MessageBoxIcon.None); //Выводим сообщение
+                Rest = true; //Задаём отдых
+                SwitchColors(RestColor ,stopButt, startButt); //Меняем цвет
+            }
+            if(TimeMin == TimeRestLimit && Rest) //Если время отдыха закончино и мы отдыхаем
+            {
+                Stop(); //Останавливаем
+                simple.Play(); //Проигрываем звук
+                stopButt.Text = "STOP"; //Меняем имя кнопки
+                MessageBox.Show("Rest is over, time to get work!", "Rest is over", MessageBoxButtons.OK, MessageBoxIcon.None); //Выводим сообщение
+                Rest = false; //Говорим о работе
+                SwitchColors(WorkColor, stopButt, startButt); //Меняем цвет
+            }
+        }
+
+        void SwitchColors(Color color, params Button[] butt)
+        {
+            this.BackColor = color; //Меняем цвет формы
+            for (int i = 0; i != butt.Length; i++) //Меняем цвет кнопок
+                butt[i].BackColor = color;
+        }
+        private void startButt_Click(object sender, EventArgs e) //Если конпка старт нажата
+        {
+            Pause = !Pause; //Реверсируем значение флага
+            if (Pause) 
+            {
+                timer1.Enabled = true;
+                startButt.Text = "PAUSE";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                startButt.Text = "START";
+            }   
+        }
+
+        private void stopButt_Click(object sender, EventArgs e) //Если кнопка стоп нажата
+        {
+            if (!Rest) //Если не отдыхаем, то останавливаем
+            {
+                Stop();
+            }
+            else //Если отдыхаем, то меняем режим
+            {
+                Stop();
+                Rest = false;
+                startButt.Text = "STOP";
+                SwitchColors(WorkColor ,stopButt, startButt);
+            }
         }
     }
 }
